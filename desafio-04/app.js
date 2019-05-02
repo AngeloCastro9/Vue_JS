b9b6e81c91fa5c6a29d236cf8b67efb7df65cdb9
angelocastro9 <castromatheus060@gmail.com>
2019-05-02 10:46:49 -0300

    dom

--- desafio-04/app.js
+++ desafio-04/app.js
@@ -3,7 +3,14 @@ new Vue({
 	data: {
 		primeiraParte: 'destaque',
 		primeiroCSS: true,
-		classe3: 'direita'
+		classe3: 'direita',
+		classe4: '',
+		cor5: '',
+		estilo5: {
+			width: '100px',
+			height: '100px'
+		},
+		width: '0',
 	},
 	methods: {
 		iniciarEfeito() {
@@ -12,7 +19,19 @@ new Vue({
 			}, 1000)
 		},
 		iniciarProgresso() {
-
+			let valor = 0
+			const temporizador = setInterval(() => {
+				valor += 5
+				this.width = `${valor}%`
+				if(valor == 100) clearInterval(temporizador)
+			}, 500)
+		},
+		setPrimeiroCSS:(event) {
+			if(event.target.value == "true") {
+				this.primeiroCSS = true
+			} else if(event.target.value == "false") {
+				this.primeiroCSS = false
+			}
 		}
 	}
 })
